8df9c9424959a7413b01768d4c55e1b1eccdafd8
sluedtke <sluedtke@gfz-potsdam.de>
2016-07-18 23:14:40 +0200

    Add return value type to docstrings

--- python_libs/libsswim/gof_python.py
+++ python_libs/libsswim/gof_python.py
@@ -16,9 +16,13 @@ import pandas as pd
 
 # --------------------------
 def read_observed(obs_file):
-    '''That function takes a single file name from the observed data and reads
+    '''
+    That function takes a single file name from the observed data and reads
     this as pandas data frame. Currently, only a single columns holds the data
-    (the second one) and the first one is the date and index column.'''
+    (the second one) and the first one is the date and index column.
+
+    Return value is a pandas dataframe
+    '''
 
     temp = pd.read_csv(obs_file, sep='\t', index_col='date',
                        parse_dates=['date'])
@@ -27,9 +31,13 @@ def read_observed(obs_file):
 
 # ----------------------------
 def read_simulated(simu_file):
-    '''That function takes a single file name from the simulated data and reads
+    '''
+    That function takes a single file name from the simulated data and reads
     this as pandas data frame. Currently, only a single columns holds the data
-    (the third one) and the first two the date and thus the index column.'''
+    (the third one) and the first two the date and thus the index column.
+
+    Return value is a pandas dataframe
+    '''
 
     dateparse = lambda y, d: pd.datetime.strptime(''.join([y, d]), '%Y%j')
 
@@ -41,8 +49,12 @@ def read_simulated(simu_file):
 
 # ---------------------------------------------
 def window_ts(pandas_df, start_date, end_date):
-    ''' This function takes a pandas dataframe, a start and an end date and
-    returns the df starting with start_date and ending with end_date.'''
+    '''
+    This function takes a pandas dataframe, a start and an end date and
+    returns the df starting with start_date and ending with end_date.
+
+    Return value is a pandas dataframe
+    '''
     # Create an index with the new date range
     temp_index = pd.date_range(start=start_date, end=end_date)
     # Use that index to clip the input data frame
@@ -52,10 +64,14 @@ def window_ts(pandas_df, start_date, end_date):
 
 # ----------------------------------------
 def obs_sim_merge(pandas_obs, pandas_sim):
-    ''' This function takes tow pandas dataframes and merges them into a single
+    '''
+    This function takes tow pandas dataframes and merges them into a single
     one. The new column names are "obs" and "sim". The join or merge will be
     based on the timestamp of the observed data set only and NA values will be
-    dropped.'''
+    dropped.
+
+    Return value is a pandas dataframe
+    '''
     temp = pd.concat([pandas_obs, pandas_sim], axis=1).dropna()
     temp.columns = ['obs', 'sim']
     return(temp)
