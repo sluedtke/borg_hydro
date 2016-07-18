5cf65e19400651796ff5cea998c7e8cb7461be52
sluedtke <sluedtke@gfz-potsdam.de>
2016-07-18 22:29:28 +0200

    Add function to merge two data frames
    By now the two dataframes have only one column of data and the simulated df is appended
    to the observed df.

--- python_libs/libsswim/gof_python.py
+++ python_libs/libsswim/gof_python.py
@@ -9,13 +9,15 @@
 
 ######################################################################
 
-# How to read the observed runoff file??
-
 import pandas as pd
 
+######################################################################
+
+
+# --------------------------
 def read_observed(obs_file):
     '''That function takes a single file name from the observed data and reads
-    this as pandas data frame. Currently, only one single columns holds the data
+    this as pandas data frame. Currently, only a single columns holds the data
     (the second one) and the first one is the date and index column.'''
 
     temp = pd.read_csv(obs_file, sep='\t', index_col='date',
@@ -23,9 +25,10 @@ def read_observed(obs_file):
     return(temp)
 
 
+# ----------------------------
 def read_simulated(simu_file):
     '''That function takes a single file name from the simulated data and reads
-    this as pandas data frame. Currently, only one single columns holds the data
+    this as pandas data frame. Currently, only a single columns holds the data
     (the third one) and the first two the date and thus the index column.'''
 
     dateparse = lambda y, d: pd.datetime.strptime(''.join([y, d]), '%Y%j')
@@ -33,4 +36,26 @@ def read_simulated(simu_file):
     temp = pd.read_csv(simu_file, sep='\t', index_col='date',
                        parse_dates={'date': ['YEAR', 'DAY']},
                        date_parser=dateparse)
+    return(temp)
+
+
+# ---------------------------------------------
+def window_ts(pandas_df, start_date, end_date):
+    ''' This function takes a pandas dataframe, a start and an end date and
+    returns the df starting with start_date and ending with end_date.'''
+    # Create an index with the new date range
+    temp_index = pd.date_range(start=start_date, end=end_date)
+    # Use that index to clip the input data frame
+    pandas_df = pandas_df.loc[temp_index]
+    return(pandas_df)
+
+
+# ----------------------------------------
+def obs_sim_merge(pandas_obs, pandas_sim):
+    ''' This function takes tow pandas dataframes and merges them into a single
+    one. The new column names are "obs" and "sim". The join or merge will be
+    based on the timestamp of the observed data set only and NA values will be
+    dropped.'''
+    temp = pd.concat([pandas_obs, pandas_sim], axis=1).dropna()
+    temp.columns = ['obs', 'sim']
     return(temp)
--- tests/test_gof.py
+++ tests/test_gof.py
@@ -8,7 +8,8 @@
 #       Author: Stefan Luedtke
 
 ######################################################################
-
+import pytest
+import pandas as pd
 from python_libs.libsswim import gof_python
 
 ######################################################################
@@ -16,11 +17,35 @@ from python_libs.libsswim import gof_python
 
 # Testing the path for the observed file
 def test_obs():
-    obs = r'./runoff_gof.dat'
-    test_obs = gof_python.read_observed(obs)
+    obs_file = r'./runoff_gof.dat'
+    obs = gof_python.read_observed(obs_file)
+    assert isinstance(obs, pd.DataFrame), 'Wrong data type'
 
 
 # Testing the path for the simulated file
 def test_sim():
-    sim = r'./mc_results/stat_dis_out_0001.csv'
-    test_sim = gof_python.read_simulated(sim)
+    sim_file = r'./mc_results/stat_dis_out_0001.csv'
+    sim = gof_python.read_simulated(sim_file)
+    assert isinstance(sim, pd.DataFrame), 'Wrong data type'
+
+
+@pytest.fixture()
+def read_obs():
+    ''' Just a fixture to read the files for further testing'''
+    obs_file = r'./runoff_gof.dat'
+    obs = gof_python.read_observed(obs_file)
+    return(obs)
+
+
+@pytest.fixture()
+def read_sim():
+    ''' Just a fixture to read the files for further testing'''
+    sim_file = r'./mc_results/stat_dis_out_0001.csv'
+    sim = gof_python.read_simulated(sim_file)
+    return(sim)
+
+
+# Merging observation and simulation into one datafram
+def test_obs_sim_merge(read_obs, read_sim):
+    temp = gof_python.obs_sim_merge(read_obs, read_sim)
+    assert isinstance(temp, pd.DataFrame), 'Wrong data type'
