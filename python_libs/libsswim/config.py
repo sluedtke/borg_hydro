9fdd6852a761d267dd98b523fd298d6aad2ea932
sluedtke <sluedtke@gfz-potsdam.de>
2016-09-01 12:02:20 +0200

    Use the json validator to check the configuration
    All settings of the model are given via a json config file. The structure of this file is
    of course nothing random but follows a schema definition. This definition is stored in
    the package as a json file and follows json-schema as described here (http://json-schema.org/).
    
    The python package 'jsonschema' is used to validate the defined schema with the user
    given config file. That is really handy.

--- python_libs/libsswim/config.py
+++ python_libs/libsswim/config.py
@@ -8,8 +8,33 @@
 ######################################################################
 
 import json
+import jsonschema
+import pkg_resources
 
 ######################################################################
+
+
+# --------------------------
+def read_json_file(json_file):
+    '''
+    Function to read a json file.
+    '''
+    with open(json_file) as json_file:
+        json_data = json.load(json_file)
+    return(json_data)
+
+
+def validate_config(model_config):
+    '''
+    Function to validate the user model configuration with the required one.
+    '''
+    # Read the schema that is stored in this directory. This will be used to
+    # validate the user given configuration.
+    schema_file = pkg_resources.resource_filename(__name__,
+                                                  './config_schema.json')
+    config_schema = read_json_file(schema_file)
+    # Validate with the user configuration given
+    jsonschema.validate(model_config, config_schema)
 
 
 # --------------------------
@@ -19,10 +44,12 @@ class user_borg_model():
     that is all a bit overkill, lets see
     '''
     def __init__(self, config_file):
-        # ''' Initialization of the model components '''
-        # read the json file with the config
-        with open(config_file) as json_file:
-            config_data = json.load(json_file)
+        ''' Initialization of the model components.'''
+        # Read json file
+        config_data = read_json_file(config_file)
+        # Validate the json schema. NOTE: the function only raises an error if
+        # the validation fails.
+        validate_config(config_data)
         # address project name
         self.pn = config_data['project_name']
         # address project path
@@ -35,4 +62,5 @@ class user_borg_model():
         # get parameter min and max
         parameter_min = [para['min'] for para in config_data['parameters']]
         parameter_max = [para['max'] for para in config_data['parameters']]
-        self.parameter_list = list(map(list, zip(parameter_min, parameter_max)))
+        self.parameter_list = list(map(list, zip(parameter_min,
+                                   parameter_max)))
--- tests/test_config.py
+++ tests/test_config.py
@@ -17,7 +17,7 @@ from python_libs.libsswim import config
 
 def test_read_config():
     '''test '''
-    config_file = './config.json'
+    config_file = './tests/test_data/config.json'
     temp = config.user_borg_model(config_file)
     assert isinstance(temp, config.user_borg_model), 'Wrong data type'
 
@@ -25,6 +25,6 @@ def test_read_config():
 @pytest.fixture()
 def read_config():
     '''test '''
-    config_file = './config.json'
+    config_file = './tests/test_data/config.json'
     temp = config.user_borg_model(config_file)
     assert isinstance(temp, config.user_borg_model), 'Wrong data type'
