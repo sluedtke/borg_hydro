dff749a1df5ac5d5d19ea350d383fc7f995c9280
sluedtke <sluedtke@gfz-potsdam.de>
2016-10-19 22:37:00 +0200

    add license file and setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='hydro_borg',
+    version='0.0.1',
+    author='Stefan Lüdtke',
+    url='https://git.gfz-potsdam.de:sluedtke/borg_hydro.git',
+    license=license,
+    packages=find_packages(exclude=('examples'))
+)
